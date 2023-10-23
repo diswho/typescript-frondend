e7f3cd5580a68d7f9e3f6e5c02cb021e07cea563
diswho <discover.who@gmail.com>
2023-10-23 13:50:57 +0700

    Remake

---
+++ src/components/resource/Resource.tsx
@@ -0,0 +1,10 @@
+import styles from "./Resource.module.css";
+const Resource = () => {
+    return (
+        <div className={styles.Resource}>
+            <h1>Resource</h1>
+            <button >Log Out</button>
+        </div>
+    );
+}
+export default Resource
